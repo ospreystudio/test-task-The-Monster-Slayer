e931ac5ab80d3652bdd355e9d0e1b647ec95edd6
ospreystudio <64425019+ospreystudio@users.noreply.github.com>
2021-07-22 18:51:33 +0400

    log complite

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
 
@@ -11,11 +12,16 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
 
         attack: function () {
-
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -24,7 +30,13 @@ new Vue({
         },
 
         specialAttack: function () {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
+
             if (this.checkWin()) {
                 return;
             }
@@ -37,6 +49,10 @@ new Vue({
             } else  {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals  for 10 '
+            });
             this.monsterAttacks();
         },
 
@@ -45,8 +61,13 @@ new Vue({
         },
 
         monsterAttacks: function () {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage
             this.checkWin()
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player ' + damage
+            });
         },
 
         calculateDamage: function (min, max) {
